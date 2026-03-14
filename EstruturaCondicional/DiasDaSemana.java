84e32b7e0500150b5644d8f43d9d7bfc03ba28d6
ghoul678 <andrebarreira@unitins.br>
2026-03-13 21:21:59 -0300

    EstruturasCondicionais

---
+++ EstruturaCondicional/DiasDaSemana.java
@@ -0,0 +1,39 @@
+package EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class DiasDaSemana {
+    public static void main(String[] args){
+        
+        int diaSemana_1;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 a 7: ");
+        diaSemana_1 = sc.nextInt();
+
+        switch (diaSemana_1) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+        
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+        }
+    }
+}
---
+++ EstruturaCondicional/OperardoraTelefonica.java
@@ -0,0 +1,26 @@
+package EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class OperardoraTelefonica {
+    
+    public static void main(String[] args){
+        int minutosconsumidos;
+        Double valorplano;
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Digite a quantidade de minutos consumidos: ");
+        minutosconsumidos = sc.nextInt();
+
+        if(minutosconsumidos <= 100){
+             valorplano = 50.00;
+             System.out.printf("O valor do plano é: R$ %.2f", valorplano);
+
+        }
+        else if(minutosconsumidos > 100 ){
+            int minutosexcedentes = minutosconsumidos -100;
+            valorplano = 50.00 + (minutosexcedentes * 2.00);
+            System.out.printf("O valor do plano é: R$ %.2f", valorplano);
+        }
+    }
+}
